b01ac4e8e3f3a9f16b48935888846fa9896b0cea
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-16 19:01:46 -0300

    Novo index2 e home

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,14 @@
 module.exports = function (app) {
 
     // home page
-    app.get('/', function (req, res) {
+    //app.get('/', function (req, res) {
         //res.render("/html/index.html", { title: 'Home Page.  ' })
         //res.write('Hello from Ruth\n')
-        res.sendFile("src/index2.html");
+    //    res.sendFile("src/index2.html");
+    //});
+    
+    app.get("/", function(req, res){
+    res.sendFile("/index2.html");
     });
 
     // chat area
