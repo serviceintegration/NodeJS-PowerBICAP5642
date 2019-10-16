a5fa374269a25aa98e2bf6c03929168a539d7053
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-16 19:18:43 -0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,17 @@
 var http = require('http');
-    var fs = require('fs');
+var res = require('res');
 
-    function onRequest(request, response) {
-        response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        fs.readFile('./index2.html', null, function (error, data) {
-            if (error) {
-                response.writeHead(404);
-                response.write('File not found!');
-            } else {
-                response.write(data);
-            }
-            response.end();
-        });
-    }
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    res.readFile('index2.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(8000);
