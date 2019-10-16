9788cb92567aea98fd3781596d497dce294a8db6
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-16 18:03:33 -0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 
     // home page
     app.get('/', function (req, res) {
-        res.render(__dirname + "/html/index.html", { title: 'Home Page.  ' })
+        res.render("/html/index.html", { title: 'Home Page.  ' })
     });
 
     // chat area
