a9ffcc732cbeb89c711b85d477fc1e12fd010e77
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-17 11:11:13 -0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,13 @@
-router.get('/', (req, res) => {
-   res.sendFile('index.html', {
-     root: './views'
-   });
+const express = require('express'),
+  es6Renderer = require('express-es6-template-engine'),
+  app = express();
+  
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+ 
+app.get('/', function(req, res) {
+  res.render('index', {locals: {title: 'Welcome!'}});
 });
+ 
+app.listen(3000);
