236359a4cb8a85b201cde6ada05a72b3d0447a2b
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-17 10:54:20 -0300

    novo server config

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 
     // home page
     app.get('/', function (req, res) {
-        res.render('index.html', { title: 'Home Page.  ' })
+        res.sendFile('./views/index.html');
     });
 
     // chat area
