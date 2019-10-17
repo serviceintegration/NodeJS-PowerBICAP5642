5fc912f707eacdcdd463857918b1d98f5b8272dd
Victor Moraes Pereira <54859697+victor-a-pereira-capgemini@users.noreply.github.com>
2019-10-17 11:13:44 -0300

    New home.js

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,20 @@
-const express = require('express'),
-  es6Renderer = require('express-es6-template-engine'),
-  app = express();
-  
-app.engine('html', es6Renderer);
-app.set('views', 'views');
-app.set('view engine', 'html');
- 
-app.get('/', function(req, res) {
-  res.render('index', {locals: {title: 'Welcome!'}});
-});
- 
-app.listen(3000);
+module.exports = function (app) {
+    const express = require('express'),
+        es6Renderer = require('express-es6-template-engine'),
+        app = express();
+
+    app.engine('html', es6Renderer);
+    app.set('views', 'views');
+    app.set('view engine', 'html');
+
+    app.get('/', function (req, res) {
+        res.render('index', {
+            locals: {
+                title: 'Welcome!'
+            }
+        });
+    });
+
+    app.listen(3000);
+
+}
